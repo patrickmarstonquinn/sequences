781ec368808ade2bb378659ca5fd711b82f92c18
patrickmarstonquinn <patrickmarstonquinn@users.noreply.github.com>
2017-01-31 21:47:54 -0600

    final

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,20 @@
 class GamesController < ApplicationController
+
   def guesses
+    @first = params["first_number"].to_i
+    @second = params["second_number"].to_i
+    @third = params["third_number"].to_i
+
+    @result = "No."
+    if @first < @second && @second < @third
+      @result = "Yes!"
+    end
+
     render("/guesses.html.erb")
+
   end
   def answer
+    @rule = params["rule"]
     render("/answer.html.erb")
   end
-  end
+end
